038882478132be1c304764ef8341778f76d42fc5
Bo Guan <bbgu@amazon.com>
2025-09-14 23:38:34 -0500

    helm token timeout 3

--- manifests/.workshop/terraform/base.tf
+++ manifests/.workshop/terraform/base.tf
@@ -81,7 +81,7 @@ provider "helm" {
   kubernetes {
     host                   = local.eks_cluster_endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
       command     = "aws"
